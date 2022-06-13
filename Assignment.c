383d4954e7ecc12fe3289b6c84116c6f884d94a7
Sh0_ <100256725+apiz23@users.noreply.github.com>
2022-06-13 14:01:49 +0800

    Update Assignment.c

--- Assignment.c
+++ Assignment.c
@@ -8,44 +8,31 @@ int longTerm(int a){
         
     //check for one WEEK or less
     else if(a <= 168){
-        if(a <= 122 && a%24 <= 5)
-            return (a/24)*11 + (a%24)*2;
-        else if (a%24 >= 6)
-            return ((a/24)+1)*11;
-	else
-	    return 60;
+        if(a <= 122 && a%24 <= 5){return (a/24)*11 + (a%24)*2;}
+        else if (a%24 >= 6){return ((a/24)+1)*11;}
+		else{return 60;}
     }
     //more than one WEEK
-    else if(a%24 == 1 || a%24 == 2)
-        return 60 + (a/24-7)*4 + (a%24)*2;
-    else if(a%24 == 0)
-        return 60 + (a/24-7)*4;
-    else
-        return 60 + (a/24-6)*4;
+    else if(a%24 == 1 || a%24 == 2){return 60 + (a/24-7)*4 + (a%24)*2;}
+    else if(a%24 == 0){return 60 + (a/24-7)*4;}
+    else{return 60 + (a/24-6)*4;}
 }
 
 float shortTerm(float b){
 
     //less than 1 hour
     if(b < 1){
-        if(b <= ((float)1/3))
-            return 0; 
-    	else if(b <= (float)2/3)
-            return 2;
-        else
-            return 3;
+        if(b <= ((float)1/3)){return 0;} 
+    	else if(b <= (float)2/3){return 2;}
+        else{return 3;}
     }
     // less than 7 hour
-    else if(b <= 6)
-        return  ceil(b)*3;
+    else if(b <= 6){return  ceil(b)*3;}
         
     //more than ONE day
-    else if((int)ceil(b)%24 == 0)
-        return (b/24)*18;
-    else if((int)ceil(b)%24 <= 6)
-        return ((int)b/24)*18 + (int)ceil(b)%24*3;
-    else
-        return ((int)ceil(b/24))*18;
+    else if((int)ceil(b)%24 == 0){return (b/24)*18;}
+    else if((int)ceil(b)%24 <= 6){return ((int)b/24)*18 + (int)ceil(b)%24*3;}
+    else{return ((int)ceil(b/24))*18;}
 }
 
 int main(){
